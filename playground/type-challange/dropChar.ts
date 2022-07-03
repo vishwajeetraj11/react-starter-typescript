cd21098ae58f38dc1a5e74040a59668503291d85
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2022-07-03 11:33:06 +0530

    drop char

---
+++ playground/type-challange/dropChar.ts
@@ -0,0 +1,52 @@
+/*
+  2070 - Drop Char
+  -------
+  by CaptainOfPhB (@CaptainOfPhB) #medium #template-literal #infer
+  
+  ### Question
+  
+  Drop a specified char from a string.
+  
+  For example:
+  
+  ```ts
+  type Butterfly = DropChar<' b u t t e r f l y ! ', ' '> // 'butterfly!'
+  ```
+  
+  > View on GitHub: https://tsch.js.org/2070
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type ReplaceAll<
+  S extends string,
+  From extends string,
+  To extends string
+> = From extends ''
+  ? S
+  : S extends `${infer Prefix}${From}${infer Suffix}`
+  ? `${ReplaceAll<Prefix, From, To>}${To}${ReplaceAll<Suffix, From, To>}`
+  : S;
+
+type DropChar<S extends string, C extends string> = ReplaceAll<S, C, ''>;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  // @ts-expect-error
+  Expect<Equal<DropChar<'butter fly!', ''>, 'butterfly!'>>,
+  Expect<Equal<DropChar<'butter fly!', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropChar<'butter fly!', '!'>, 'butter fly'>>,
+  Expect<Equal<DropChar<'    butter fly!        ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', 'b'>, '  u t t e r f l y ! '>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', 't'>, ' b u   e r f l y ! '>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/2070/answer
+  > View solutions: https://tsch.js.org/2070/solutions
+  > More Challenges: https://tsch.js.org
+*/
